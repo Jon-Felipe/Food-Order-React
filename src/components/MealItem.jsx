6ae895672e9fdc51a355e0c81adfbd68362bc12c
Jon-Felipe <rodriguesjf7@gmail.com>
2025-06-05 15:22:44 +0200

    formatting numbers as currency

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,3 +1,6 @@
+// extras
+import { currencyFormatter } from '../utils/formatting';
+
 function MealItem({ meal }) {
   return (
     <li className='meal-item'>
@@ -5,7 +8,9 @@ function MealItem({ meal }) {
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p className='meal-item-price'>{meal.price}</p>
+          <p className='meal-item-price'>
+            {currencyFormatter.format(meal.price)}
+          </p>
           <p className='meal-item-description'>{meal.description}</p>
         </div>
         <p className='meal-item-actions'>
